f3ce10fc56c65aa5373c3fe441d43548b652f878
Adrian de Lozar Moreno <adrianlozar@172-15-71-63.lightspeed.austtx.sbcglobal.net>
2019-02-12 21:13:53 +0100

    First Commit

---
+++ code.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    setInterval(function(){
+        $('#fecha').html(new Date());
+    }, 1000);
+
+    $(function(){
+        
+    
+
+    
+ 
+
+    });
+
+
+
+
+
+    
+}
+    );
+
